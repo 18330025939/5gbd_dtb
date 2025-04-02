61a10a8010e13ae1290fe5772c81133f44a9b7c8
lrj <915581696@qq.com>
2025-04-02 16:22:13 +0800

    1.Edit compilation error;

--- kt_5gbd_dtb/src/main.c
+++ kt_5gbd_dtb/src/main.c
@@ -18,34 +18,33 @@ void signal_handler(evutil_socket_t fd, short events, void *arg)
 
 int main(int argc, char ** args)
 {
-    // CloundCommContext *cloud_ctx = NULL;
+    CloundCommContext *cloud_ctx = NULL;
 
-    // cloud_ctx = (CloundCommContext*)malloc(sizeof(CloundCommContext));
-    // if (cloud_ctx == NULL) {
-    //     exit(1);
-    // }
-    // memset(cloud_ctx, 0, sizeof(CloundCommContext));
-    // clound_comm_init(cloud_ctx);
+    cloud_ctx = (CloundCommContext*)malloc(sizeof(CloundCommContext));
+    if (cloud_ctx == NULL) {
+        exit(1);
+    }
+    memset(cloud_ctx, 0, sizeof(CloundCommContext));
+    clound_comm_init(cloud_ctx);
 
-    // struct event_base *base = event_base_new();
-    // if (!base) {
-    //     perror("event_base_new");
-    //     exit(1);
-    // }
-    // struct event *signal = event_new(base, SIGINT, 0, signal_handler, base);
-    // if (!signal) {
-    //     perror("event_new");
-    //     event_base_free(base);
-    //     exit(1);
-    // }
-    // event_add(signal, NULL);
+    struct event_base *base = event_base_new();
+    if (!base) {
+        perror("event_base_new");
+        exit(1);
+    }
+    struct event *signal = event_new(base, SIGINT, 0, signal_handler, base);
+    if (!signal) {
+        perror("event_new");
+        event_base_free(base);
+        exit(1);
+    }
+    event_add(signal, NULL);
 
-    // event_base_dispatch(base);
+    event_base_dispatch(base);
 
-    // event_free(signal);
-    // event_base_free(base);
-    // clound_comm_init(cloud_ctx);
+    event_free(signal);
+    event_base_free(base);
+    clound_comm_init(cloud_ctx);
 
-    printf("main...\n");
     return 0;
 }
