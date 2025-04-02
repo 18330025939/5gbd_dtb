a2e22c684a9c85082efe99dcf42de0b2cf7439bb
lrj <915581696@qq.com>
2025-04-02 16:11:16 +0800

    1.Edit compilation error;

--- kt_5gbd_dtb/src/main.c
+++ kt_5gbd_dtb/src/main.c
@@ -7,44 +7,45 @@
 #include <event2/event.h>
 #include "cloud_comm.h"
 
-
 void signal_handler(evutil_socket_t fd, short events, void *arg)
 {
-    struct event_base *base = (struct event_base *)arg;
-    printf("收到退出信号，准备退出...\n");
+    struct event_base *base = NULL;
+
+    base = (struct event_base *)arg;
+    printf("Received SIGINT,quit...\n");
     event_base_loopexit(base, NULL);
 }
 
 int main()
 {
-    CloundCommContext *cloud_ctx = NULL;
+    // CloundCommContext *cloud_ctx = NULL;
 
-    cloud_ctx = (CloundCommContext*)malloc(sizeof(CloundCommContext));
-    if (cloud_ctx == NULL) {
-        exit(1);
-    }
-    memset(cloud_ctx, 0, sizeof(CloundCommContext));
-    clound_comm_init(cloud_ctx);
+    // cloud_ctx = (CloundCommContext*)malloc(sizeof(CloundCommContext));
+    // if (cloud_ctx == NULL) {
+    //     exit(1);
+    // }
+    // memset(cloud_ctx, 0, sizeof(CloundCommContext));
+    // clound_comm_init(cloud_ctx);
 
-    struct event_base *base = event_base_new();
-    if (!base) {
-        perror("event_base_new");
-        exit(1);
-    }
-    struct event *signal = event_new(base, SIGINT, 0, signal_handler, base);
-    if (!signal) {
-        perror("event_new");
-        event_base_free(base);
-        exit(1);
-    }
-    event_add(signal, NULL);
+    // struct event_base *base = event_base_new();
+    // if (!base) {
+    //     perror("event_base_new");
+    //     exit(1);
+    // }
+    // struct event *signal = event_new(base, SIGINT, 0, signal_handler, base);
+    // if (!signal) {
+    //     perror("event_new");
+    //     event_base_free(base);
+    //     exit(1);
+    // }
+    // event_add(signal, NULL);
 
-    event_base_dispatch(base);
+    // event_base_dispatch(base);
 
-    event_free(signal);
-    event_base_free(base);
-    clound_comm_init(cloud_ctx);
+    // event_free(signal);
+    // event_base_free(base);
+    // clound_comm_init(cloud_ctx);
 
+    printf("main...\n");
     return 0;
 }
-
