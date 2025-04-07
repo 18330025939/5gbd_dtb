529f09d61c81ae8ee003b09779ed84dbe2841759
lrj <915581696@qq.com>
2025-04-07 16:19:13 +0800

    1.Edit compilation error;

--- kt_5gbd_dtb/src/services/cloud_comm.c
+++ kt_5gbd_dtb/src/services/cloud_comm.c
@@ -247,6 +247,7 @@ void nav_data_msg_task_cb(evutil_socket_t fd, short event, void *arg)
     CloundCommContext *ctx = (CloundCommContext *)arg;
     laneTo_read_nav_data(ctx->laneTo);
     // ThreadSafeQueue *send_queue = &ctx->queue;
+    memset(buf, 0, sizeof(buf));
     hdr = (MsgFramHdr *)buf;
     hdr->usHdr = MSG_DATA_FRAM_HDR;
     hdr->ucSign = MSG_SIGN_TRANS_NAV_DATA;
