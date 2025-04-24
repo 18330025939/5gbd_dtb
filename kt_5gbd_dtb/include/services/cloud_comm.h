c7b7afd61c3d78293ea9496df06e0167b7e77da5
lrj <915581696@qq.com>
2025-04-24 11:23:32 +0800

    1.Edit compilation error

--- kt_5gbd_dtb/include/services/cloud_comm.h
+++ kt_5gbd_dtb/include/services/cloud_comm.h
@@ -136,10 +136,10 @@ struct MsgProcInf {
     uint8_t sign;
     int (*pFuncEntry)(void *);
     int (*pFuncCb)(void *);
-} ;
+} __attribute__((aligned(8)));
 
 #define REGISTER_MESSAGE_PROCESSINFG_INTERFACE(name, msg_sign, func_entry, func_cb)\
-    __attribute__((used, __section__("messgae_processing"))) static struct MsgProcInf meg_proc_##name#_inf = { \
+    __attribute__((used, __section__("messgae_processing"))) static struct MsgProcInf meg_proc_##name = { \
         .sign = msg_sign, \
         .pFuncEntry = func_entry, \
         .pFuncCb = func_cb \
