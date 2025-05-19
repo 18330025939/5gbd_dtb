38ac5ca9b89a81aa79d680bffa295eb8d220744d
lrj <915581696@qq.com>
2025-05-19 10:54:38 +0800

    1.Edit log.cpp

--- kt_5gbd_dtb/include/units/log.h
+++ kt_5gbd_dtb/include/units/log.h
@@ -1,19 +1,40 @@
-#ifndef __SPDLOG_C_H
-#define __SPDLOG_C_H
+#ifndef SPDLOG_C_H
+#define SPDLOG_C_H
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-int init_spdlog(const char* log_file_path, int max_size, int max_files);
-void log_info(const char* format, ...);
-void log_error(const char* format, ...);
-void log_warning(const char* format, ...);
-void log_debugconst char* format, ...);
+typedef struct st_spdlog_logger {
+    std::shared_ptr<spdlog::logger> logger;
+} spdlog_logger;
+
+typedef enum {
+    SPDLOG_LEVEL_TRACE = 0,
+    SPDLOG_LEVEL_DEBUG,
+    SPDLOG_LEVEL_INFO,
+    SPDLOG_LEVEL_WARN,
+    SPDLOG_LEVEL_ERROR,
+    SPDLOG_LEVEL_CRITICAL,
+    SPDLOG_LEVEL_OFF
+} spdlog_level;
+
+spdlog_logger* spdlog_init(const char* logger_name, const char* filename);
+void spdlog_shutdown(spdlog_logger* logger);
+
+void spdlog_set_level(spdlog_logger* logger, spdlog_level level);
+
+void spdlog_log(spdlog_logger* logger, spdlog_level level, const char* fmt, ...);
+
+#define spdlog_trace(logger, ...) spdlog_log(logger, SPDLOG_LEVEL_TRACE, __VA_ARGS__)
+#define spdlog_debug(logger, ...) spdlog_log(logger, SPDLOG_LEVEL_DEBUG, __VA_ARGS__)
+#define spdlog_info(logger, ...)  spdlog_log(logger, SPDLOG_LEVEL_INFO, __VA_ARGS__)
+#define spdlog_warn(logger, ...)  spdlog_log(logger, SPDLOG_LEVEL_WARN, __VA_ARGS__)
+#define spdlog_error(logger, ...) spdlog_log(logger, SPDLOG_LEVEL_ERROR, __VA_ARGS__)
+#define spdlog_critical(logger, ...) spdlog_log(logger, SPDLOG_LEVEL_CRITICAL, __VA_ARGS__)
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif // __SPDLOG_C_H
-
+#endif // SPDLOG_C_H
--- kt_5gbd_dtb/src/untils/log.cpp
+++ kt_5gbd_dtb/src/untils/log.cpp
@@ -1,10 +1,10 @@
+#if 0
 #include <string>
 #include <iostream>
 #include <cstdarg>  
-#include <spdlog.h>
-#include <sinks/stdout_color_sinks.h>
-#include <sinks/rotating_file_sink.h>
-
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/rotating_file_sink.h>
 
 // 定义一个全局日志器
 static std::shared_ptr<spdlog::logger> g_rotating_logger;
@@ -64,4 +64,74 @@ extern "C" void log_debug(const char* format, ...)
         g_rotating_logger->debug(format, args);
         va_end(args);
     }
+}
+#endif 
+// spdlog_c.cpp
+#include <cstdarg>
+#include <cstdio>
+#include <vector>
+#include <memory>
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include "spdlog_c.h"
+
+namespace {
+    std::string format_message(const char* fmt, va_list args)
+    {
+        va_list args_copy;
+        va_copy(args_copy, args);
+        int size = vsnprintf(nullptr, 0, fmt, args_copy);
+        va_end(args_copy);
+
+        if (size < 0) return "";
+        
+        std::vector<char> buf(size + 1);
+        vsnprintf(buf.data(), buf.size(), fmt, args);
+        return std::string(buf.data(), size);
+    }
+}
+
+spdlog_logger_t* spdlog_init(const char* logger_name, const char* filename)
+{
+    try {
+        std::shared_ptr<spdlog::logger> logger;
+        if (filename) {
+            logger = spdlog::basic_logger_mt(logger_name, filename);
+        } else {
+            logger = spdlog::stdout_color_mt(logger_name);
+        }
+        logger->set_level(spdlog::level::trace);
+        return new spdlog_logger{logger};
+    } catch (const spdlog::spdlog_ex& ex) {
+        fprintf(stderr, "spdlog init failed: %s\n", ex.what());
+        return nullptr;
+    }
+}
+
+void spdlog_shutdown(spdlog_logger* logger)
+{
+    if (logger) {
+        spdlog::drop(logger->logger->name());
+        delete logger;
+    }
+}
+
+void spdlog_set_level(spdlog_logger* logger, spdlog_level level)
+{
+    if (logger && logger->logger) {
+        logger->logger->set_level(static_cast<spdlog::level::level_enum>(level));
+    }
+}
+
+void spdlog_log(spdlog_logger* logger, spdlog_level level, const char* fmt, ...)
+{
+    if (!logger || !logger->logger) return;
+
+    va_list args;
+    va_start(args, fmt);
+    std::string msg = format_message(fmt, args);
+    va_end(args);
+
+    logger->logger->log(static_cast<spdlog::level::level_enum>(level), msg);
 }
