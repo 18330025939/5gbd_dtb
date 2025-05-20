ba4b19f59a1cc319b6d3a4c235c248303c517439
lrj <915581696@qq.com>
2025-05-20 11:46:55 +0800

    1.Edit mqtt log error.

--- kt_5gbd_dtb/src/net/mqtt_client.c
+++ kt_5gbd_dtb/src/net/mqtt_client.c
@@ -112,7 +112,7 @@ int mqtt_publish(AsyncMQTTClient* client, const char* topic, const void* payload
     if (str != NULL) {
         str[0] = '\0';
         for (int i = 0; i < len; i++) {
-            sprintf(str + strlen(str), "%hhu ", arr[i]);
+            sprintf(str + strlen(str), "%hhu ", payload[i]);
         }
 
         if (strlen(str) > 0) {
